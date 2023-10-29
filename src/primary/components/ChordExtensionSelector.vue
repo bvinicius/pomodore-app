dfb93b3a8b6332ee5d459fc6918e470e2ab4ddac
bvinicius <vinicius.bazanella1@gmail.com>
2023-10-29 20:41:46 +0000

    Adds more extensions

--- src/primary/components/ChordExtensionSelector.vue
+++ src/primary/components/ChordExtensionSelector.vue
@@ -5,7 +5,7 @@
             :key="ext"
             :checked="notesStore.intervals.includes(toNumber(ext))"
             class="py-2 px-4 rounded-sm text-white"
-            @click="notesStore.toggleExtension(toNumber(ext))"
+            @click="() => onToggleExtension(toNumber(ext))"
         >
             {{ ext }}
         </ToggleButton>
@@ -16,9 +16,30 @@
 import { useNotesStore } from '../infrastructure/store/NotesStore';
 import ToggleButton from './atoms/ToggleButton.vue';
 
-const CHORD_EXTENSIONS = ['1st', '3rd', '5th', '7th', '9th'];
+const MAX_SIMULTANEOUS_EXTENSIONS = 5;
+const CHORD_EXTENSIONS = [
+    '1st',
+    '2nd',
+    '3rd',
+    '4th',
+    '5th',
+    '6th',
+    '7th',
+    '8th',
+    '9th'
+];
 
 const notesStore = useNotesStore();
+
+const onToggleExtension = (extension: number) => {
+    if (notesStore.intervals.length >= MAX_SIMULTANEOUS_EXTENSIONS) {
+        if (notesStore.intervals.includes(extension)) {
+            notesStore.toggleExtension(extension);
+        }
+        return;
+    }
+    notesStore.toggleExtension(extension);
+};
 
 const toNumber = (extension: string) => Number(extension.slice(0, -2));
 </script>
