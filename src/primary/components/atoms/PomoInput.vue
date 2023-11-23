e0e326611ab20f7a8448ae1244e30b0e474c552c
bvinicius <vinicius.bazanella1@gmail.com>
2023-11-23 08:51:23 +0000

    Fixes v-model in PomoInput

--- src/primary/components/atoms/PomoInput.vue
+++ src/primary/components/atoms/PomoInput.vue
@@ -1,7 +1,25 @@
 <template>
     <input
-        v-bind="$attrs"
+        v-model="vModel"
         type="text"
         class="bg-primary-container-400 border border-gray-777 text-on-container-300 py-1 px-2 rounded-md focus-visible:outline-blue"
     />
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{ modelValue?: unknown }>();
+const emit = defineEmits<{
+    (e: 'update:model-value', v: unknown): void;
+}>();
+
+const vModel = computed({
+    get() {
+        return props.modelValue;
+    },
+    set(v: unknown) {
+        emit('update:model-value', v);
+    }
+});
+</script>
