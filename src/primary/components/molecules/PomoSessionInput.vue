067953ba2c6c259a18d0a9dfde9a76eced1d2afe
bvinicius <vinicius.bazanella1@gmail.com>
2023-11-25 09:53:49 +0000

    WIP - Styles pomo input

--- src/primary/components/molecules/PomoSessionInput.vue
+++ src/primary/components/molecules/PomoSessionInput.vue
@@ -1,29 +1,33 @@
 <template>
-    <div class="w-32 relative">
-        <div
-            class="rounded-full bg-primary-container-400 w-8 h-8 text-white font-bold text-center"
-            @click="() => pomoInputWrapper?.stepUp()"
-        >
-            ^
+    <div class="flex items-center gap-2">
+        <div class="flex flex-col gap-2">
+            <div
+                class="rounded-full bg-primary-container-400 w-8 h-8 text-white font-bold text-center"
+                @click="() => pomoInputWrapper?.stepUp()"
+            >
+                ^
+            </div>
+            <div
+                class="rounded-full bg-primary-container-400 w-8 h-8 text-white font-bold text-center rotate-180"
+                @click="() => pomoInputWrapper?.stepDown()"
+            >
+                ^
+            </div>
         </div>
-        <div
-            class="rounded-full bg-primary-container-400 w-8 h-8 text-white font-bold text-center rotate-180"
-            @click="() => pomoInputWrapper?.stepDown()"
-        >
-            ^
+        <div class="w-32 relative">
+            <PomoInput
+                ref="pomoInputWrapper"
+                v-model="vModel"
+                type="number"
+                class="w-full py-3 font-semibold text-6xl !text-primary-500 text-center rounded-xl pb-6 pomp-input spin-buttons-none"
+                :label="hint"
+            />
+            <span
+                class="font-semibold text-on-container-400 absolute bottom-0 left-0 right-0 text-center pb-2"
+            >
+                {{ hint }}
+            </span>
         </div>
-        <PomoInput
-            ref="pomoInputWrapper"
-            v-model="vModel"
-            type="number"
-            class="w-full py-3 font-semibold text-6xl !text-primary-500 text-center rounded-xl pb-6 pomp-input"
-            :label="hint"
-        />
-        <span
-            class="font-semibold text-on-container-400 absolute bottom-0 left-0 right-0 text-center pb-2"
-        >
-            {{ hint }}
-        </span>
     </div>
 </template>
 
