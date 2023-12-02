faed2cc47008e3fbb8253c0b51745057c74bcb02
bvinicius <vinicius.bazanella1@gmail.com>
2023-12-02 22:11:32 +0000

    Adds button variant to PomoIcon

--- src/primary/components/atoms/PomoIcon.vue
+++ src/primary/components/atoms/PomoIcon.vue
@@ -1,9 +1,14 @@
 <template>
-    <span class="material-icons">{{ name }}</span>
+    <span
+        class="material-icons"
+        :class="{ 'cursor-pointer hover:opacity-70': button }"
+        >{{ name }}</span
+    >
 </template>
 
 <script setup lang="ts">
 defineProps<{
     name: string;
+    button?: boolean;
 }>();
 </script>
