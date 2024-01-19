f6519d250d2cb4dffcaf7cd236ffdc356ccfc83a
bvinicius <vinicius.bazanella1@gmail.com>
2024-01-19 19:02:50 +0000

    Adds vercel metrics component

--- src/primary/App.vue
+++ src/primary/App.vue
@@ -2,10 +2,13 @@
     <div class="w-screen h-screen px-12 py-8 md:px-24 md:py-12">
         <router-view></router-view>
     </div>
+
+    <speed-insights></speed-insights>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 import { usePomoStore } from '@/primary/infrastructure/store/pomoStore';
 import { usePomoRunner } from '@/primary/infrastructure/composables/pomoRunner';
 
