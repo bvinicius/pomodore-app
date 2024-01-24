f8eed1f1c67e3f338d5ce08378cf213f51427329
bvinicius <vinicius.bazanella1@gmail.com>
2024-01-24 10:26:09 +0000

    logs

--- src/primary/main.ts
+++ src/primary/main.ts
@@ -7,6 +7,17 @@ import App from '@/primary/App.vue';
 import router from '@/primary/infrastructure/router';
 import { provide } from '@/primary/infrastructure/dependency-injection';
 
+console.log('VITE_SUPABASE_URL', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_API_KEY', import.meta.env.VITE_SUPABASE_API_KEY);
+console.log(
+    'VITE_SUPABASE_API_SECRET',
+    import.meta.env.VITE_SUPABASE_API_SECRET
+);
+console.log(
+    'VITE_SUPABASE_JWT_SECRET',
+    import.meta.env.VITE_SUPABASE_JWT_SECRET
+);
+
 const app = createApp(App);
 
 provide(app);
